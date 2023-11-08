b21c29c6da119e08ff1f2c243a8044f74e62c89e
Maxim Postika <lakkiluk.m@gmail.com>
2023-11-08 16:15:11 +0300

    index.js add_3

--- scripts/index.js
+++ scripts/index.js
@@ -2,7 +2,7 @@ const template = document.querySelector("#card-template").content;
 const cardContainer = document.querySelector(".places__list");
 const cardElement = template.querySelector(".places__item.card");
 
-function NewCard(cardData, handleCardDelete) {
+function addCard(cardData, handleCardDelete) {
   cardData.forEach(function (item) {
     const newCard = cardElement.cloneNode(true);
     newCard.querySelector(".card__image").src = item.link;
